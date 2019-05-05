65980d486f9f2c9f455d5ed7d53db5f193de1bda
Olesia <5otaisora@gmail.com>
2019-05-05 20:47:27 +0300

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,129 +1,272 @@
+package.cpath = package.cpath .. ";" .. getWorkingFolder() .. "\\?51.dll"
+require( "iuplua" )
+
 -- Параметры
-p_classcode = "TQBR" -- Код класса
-p_seccode = "SBER" -- Код бумаги 
-p_interval = INTERVAL_M1 -- Временной интервал
+--p_classcode = "TQBR" -- Код класса
+--p_seccode = "SBER" -- Код бумаги 
+--p_interval = INTERVAL_M1 -- Временной интервал
 p_bars = 140 -- Количество баров
 p_range = 5 -- Размер фрактала
 p_MA_f = 5 -- Быстрая MA (с меньшим периодом)
 p_MA_a = 20 -- Cредняя MA (с промежуточным периодом)
 p_MA_s = 80 -- Медленная MA (с большим периодом)
 
+p_interval_start = INTERVAL_M1
+p_interval_finish = INTERVAL_H1
+
 --count -- Число свечей в источнике данных (= индексу последней свечки)
 --trend_dow -- Последний зафиксированный тренд по Доу
 --trend_williams -- Последний зафиксированный тренд по Вильямсу
---trend_ma
+--trend_ma -- Тренд по скользящим средним
+
+results = {} -- Конечная таблица с данными анализа
 
 fileName = "log.txt" -- Название файла по умолчанию
 filePath = "C:\\Users\\5otai\\Desktop\\" -- Путь к файлам
-
-is_run = true  
 
 intervals = {INTERVAL_TICK, INTERVAL_M1, INTERVAL_M2, INTERVAL_M3, INTERVAL_M4,
 	INTERVAL_M5, INTERVAL_M6, INTERVAL_M10, INTERVAL_M15, INTERVAL_M20, INTERVAL_M30,
 	INTERVAL_H1, INTERVAL_H2, INTERVAL_H4, INTERVAL_D1, INTERVAL_W1, INTERVAL_MN1}	
 
-
-if(p_range % 2 == 0) then
-	p_range = p_range + 1
-end
-
-if(p_range < 3) then 
-	p_range = 3
-end
-
--- Определяем центр фрактала (смещение)	
-center = math.floor(p_range/2)
-
-function handleNewKindle (index) -- Обработка новой свечи
-	if index == count + 1 then -- При окончательном формировании последней свечи и появлении новой
-		local fractals = getFrac()
-		local new_trend_dow = defTrendDow(fractals)
-		local new_trend_williams = defTrendWilliams(fractals)
-		if new_trend_dow ~= trend_dow or new_trend_williams ~= trend_williams then
-			trend_dow = new_trend_dow
-			trend_williams = new_trend_williams
-			message("Направление тренда изменилось! Теперь он\n"..
-			"По Доу: "..trend_dow.."\n"..
-			"По Вильямсу: "..trend_williams)
-		end
-	end
-end
-
-function SMA(index, period, prev_SMA) -- Простая скользящая средняя (SMA, Simple Moving Average)
-	if prev_SMA == nil then
-		local sum = 0
-		for i = index - period + 1, index do
-			sum = sum + ds:C(i)
-		end
-		return sum/period
-	else
-		return prev_SMA - (ds:C(index - period) - ds:C(index))/period
-	end
-end
-
-function defTrendMA()
-	local trend = "отсутствует"
-	local MA_f, MA_a, MA_s
-	for i = count - p_bars, count - 1 do
-		MA_f = SMA(i, p_MA_f, MA_f)
-		MA_a = SMA(i, p_MA_a, MA_a)
-		MA_s = SMA(i, p_MA_s, MA_s)
-		if MA_a <= MA_f and MA_a >= MA_s then -- Наблюдается тренд, если средняя MA располагается между двумя другими
-			if MA_f > math.max(MA_a, MA_s) and trend ~="восходящий" then
-				trend ="восходящий"
-			elseif MA_f < math.min(MA_a, MA_s) and trend ~="нисходящий" then
-				trend ="нисходящий"
-			end 
-		elseif trend ~="горизонтальный" then
-			trend ="горизонтальный"
-		end				
-	end
-	return trend
-end
-
 function main() -- Основной поток программы
-	-- Создаем таблицу со всеми свечами нужного интервала, класса и кода	
-	ds, error_desc = CreateDataSource(p_classcode, p_seccode, p_interval)	
-	-- Ограничиваем количество попыток (времени) ожидания получения данных от сервера
-	local try_count = 0
-	-- Ждем пока не получим данные от сервера,
-	--	либо пока не закончится время ожидания (количество попыток)
-	while ds == nil and try_count < 100 do
-		sleep(100)
-		try_count = try_count + 1
+	saveToFile("The program is running! "..os.date("%b %d %H:%M:%S").."\n")
+	classcode = iup.text{
+					multiline = "NO",
+					expand = "NO",
+					value = "TQBR"
+					}
+	seccode = iup.text{
+					multiline = "NO",
+					expand = "NO",
+					value = "SBER"}
+	dlg = iup.dialog
+		{
+		iup.gridbox
+			{
+			iup.label {title="Seccode: "},
+			seccode,
+			iup.label {title="Classcode: "},
+			classcode,
+			iup.button{title="OK", action = btn_ok_click},
+			iup.button{title="Exit", action = btn_exit_click},
+			numdiv = "2", 
+			EXPANDCHILDREN = "YES", 
+			ORIENTATION = "HORIZONTAL",
+			GAPLIN = "10",
+			GAPCOL = "10",
+			margin = "10x10"
+			}
+		;title="Main", 
+		font="Helvetica, 14",
+		size="150x80"
+		}
+	dlg:showxy(iup.CENTER,iup.CENTER)
+
+	-- to be able to run this script inside another context
+	if (iup.MainLoopLevel()==0) then
+		iup.MainLoop()
+		iup.Close()
 	end
-	ds:SetUpdateCallback(handleNewKindle) -- Задаем свой обработчик при поступлении новой информации из источника
-	-- Если от сервера пришла ошибка, то выведем ее и прервем выполнение
-	if error_desc ~= nil and error_desc ~= "" then
-		message("Ошибка получения таблицы свечей:" .. error_desc)
-		return 0
-	elseif ds:Size() < p_bars + p_range then
-		message("Недостаточно свечей! "..tostring(ds:Size()))
-		return 0
-	else
-		saveToFile("The program is running! "..os.date("%b %d %H:%M:%S").."\n") 
-		local fractals = getFrac()
-		trend_dow = defTrendDow(fractals)
-		trend_williams = defTrendWilliams(fractals)
-		trend_ma = defTrendMA()
-		count = ds:Size()
-		message("Получено свечей: "..tostring(count).."\n\n"..
-			"\tТренд\n\n"..
-			"По Доу: "..trend_dow.."\n"..
-			"По Вильямсу: "..trend_williams.."\n"..
-			"По скользящим средним: "..trend_ma)
+end
+
+function getResults() -- Получение результатов анализа
+	
+	if(p_range % 2 == 0) then
+		p_range = p_range + 1
 	end
-	while is_run do
+
+	if(p_range < 3) then 
+		p_range = 3
+	end
+
+	-- Определяем центр фрактала (смещение)	
+	center = math.floor(p_range/2)
+
+	local file = io.open(filePath.."Results.json","w")
+	file:write("{\n\t\"results\":[\n")
+	for i = getIndexByValue(intervals,p_interval_start), getIndexByValue(intervals,p_interval_finish) do
+		local interval = intervals[i]
+		-- Создаем таблицу со всеми свечами нужного интервала, класса и кода	
+		ds, error_desc = CreateDataSource(p_classcode, p_seccode, interval)	
+		-- Ограничиваем количество попыток (времени) ожидания получения данных от сервера
+		local try_count = 0
+		-- Ждем пока не получим данные от сервера,
+		--	либо пока не закончится время ожидания (количество попыток)
+		while ds == nil and try_count < 100 do
+			sleep(300)
+			try_count = try_count + 1
+		end
+		--ds:SetUpdateCallback(handleNewKindle) -- Задаем свой обработчик при поступлении новой информации из источника
+		-- Если от сервера пришла ошибка, то выведем ее и прервем выполнение
+		if error_desc ~= nil and error_desc ~= "" then
+			message("Ошибка получения таблицы свечей:" .. error_desc)
+			return 0
+		elseif ds:Size() < p_bars + p_range then
+			message("Недостаточно свечей! "..tostring(ds:Size()))
+			return 0
+		else 
+			local fractals = getFrac(interval)
+			local trend_dow = defTrendDow(fractals)
+			local trend_williams = defTrendWilliams(fractals)
+			local trend_ma = defTrendMA()
+			count = ds:Size()
+			local str = "Интервал: "..tostring(interval).." min \n\tТренд\n\n".."По Доу: "..trend_dow.."\n".."По Вильямсу: "..trend_williams.."\n".."По скользящим средним: "..trend_ma.."\n\n"
+			saveToFile(str,"Results.txt")	
+			
+			local json_str = "\t\t{\n\t\t\t\"interval\":"..tostring(interval)..
+			",\n\t\t\t\"methods\":[\n\t\t\t\t{\n\t\t\t\t\t\"Dow\":\""..trend_dow..
+			"\"\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"Williams\":\""..trend_williams..
+			"\"\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"MA\":\""..trend_ma.."\"\n\t\t\t\t}\n\t\t\t]\n\t\t},\n"
+			file:write(json_str)	
+			file:flush()
+			results[#results+1] = { 
+								["interval"] = interval,
+								["methods"] = {
+												["ma"] = trend_ma,
+												["dow"] = trend_dow,
+												["williams"] = trend_williams
+											}
+							}
+		end		
 	end
 	ds:Close()
+	file:seek("end",-3) -- Убирает последнюю запятую
+	file:write(" \n\t]\n}")
+	file:close()
 end
 
-function OnStop() -- При остановке скрипта
-    is_run = false
+function btn_exit_click(self) -- Обработка нажатия кнопки "Exit"
+  return iup.CLOSE  
+end
+
+function btn_ok_click(self) -- Обработка нажатия кнопки "OK"
+	p_classcode = classcode.value
+	p_seccode = seccode.value
+	getResults()
+	
+	counter = {
+			dow = {
+			up = 0,
+			down = 0
+			},
+			williams = {
+			up = 0,
+			down = 0,
+			flat = 0
+			},
+			ma = {
+			up = 0,
+			down = 0,
+			flat = 0
+			}}
+	grd = iup.gridbox{numdiv = "9", 
+		EXPANDCHILDREN = "YES", 
+		ORIENTATION = "HORIZONTAL",
+		GAPLIN = "10",
+		GAPCOL = "10",
+		margin = "10x10",
+		--EXPAND = "NO",
+		--HOMOGENEOUSCOL  = "YES"
+		}
+	grd:append(iup.label{title = "Interval\n\nDow\n\nWilliams\n\nMA"})
+	for i = 1,7 do
+		local lbl = iup.label{title = tostring(results[i].interval).." min\n\n"..
+		results[i].methods.dow.."\n\n"..results[i].methods.williams.."\n\n"..results[i].methods.ma}
+		grd:append(lbl)
+		
+		if results[i].methods.dow == "восходящий" then
+			counter.dow.up = counter.dow.up + 1
+		else
+			counter.dow.down = counter.dow.down + 1
+		end
+		
+		if results[i].methods.williams == "восходящий" then
+			counter.williams.up = counter.williams.up + 1
+		elseif results[i].methods.williams == "нисходящий" then
+			counter.williams.down = counter.williams.down + 1
+		else
+			counter.williams.flat = counter.williams.flat + 1
+		end
+		
+		if results[i].methods.ma == "восходящий" then
+			counter.ma.up = counter.ma.up + 1
+		elseif results[i].methods.ma == "нисходящий" then
+			counter.ma.down = counter.ma.down + 1
+		else
+			counter.ma.flat = counter.ma.flat + 1
+		end
+		
+	end
+	grd:append(iup.label{title = "Total (up/down/flat)\n\n"..counter.dow.up.."/"..counter.dow.down.."\n\n"..
+	counter.williams.up.."/"..counter.williams.down.."/"..counter.williams.flat.."\n\n"..
+	counter.ma.up.."/"..counter.ma.down.."/"..counter.ma.flat})
+	
+	counter = {
+			dow = {
+			up = 0,
+			down = 0
+			},
+			williams = {
+			up = 0,
+			down = 0,
+			flat = 0
+			},
+			ma = {
+			up = 0,
+			down = 0,
+			flat = 0
+			}}
+	grd:append(iup.label{title = "Interval\n\nDow\n\nWilliams\n\nMA"})
+	for i = 5,11 do
+		local lbl = iup.label{title = tostring(results[i].interval).." min\n\n"..
+		results[i].methods.dow.."\n\n"..results[i].methods.williams.."\n\n"..results[i].methods.ma}
+		grd:append(lbl)
+		
+		if results[i].methods.dow == "восходящий" then
+			counter.dow.up = counter.dow.up + 1
+		else
+			counter.dow.down = counter.dow.down + 1
+		end
+		
+		if results[i].methods.williams == "восходящий" then
+			counter.williams.up = counter.williams.up + 1
+		elseif results[i].methods.williams == "нисходящий" then
+			counter.williams.down = counter.williams.down + 1
+		else
+			counter.williams.flat = counter.williams.flat + 1
+		end
+		
+		if results[i].methods.ma == "восходящий" then
+			counter.ma.up = counter.ma.up + 1
+		elseif results[i].methods.ma == "нисходящий" then
+			counter.ma.down = counter.ma.down + 1
+		else
+			counter.ma.flat = counter.ma.flat + 1
+		end
+	end
+	grd:append(iup.label{title = "Total (up/down/flat)\n\n"..counter.dow.up.."/"..counter.dow.down.."\n\n"..
+	counter.williams.up.."/"..counter.williams.down.."/"..counter.williams.flat.."\n\n"..
+	counter.ma.up.."/"..counter.ma.down.."/"..counter.ma.flat})
+	sec_dlg = iup.dialog
+		{ grd
+		--[[iup.gridbox
+			{
+			iup.button{title="Exit", action = btn_exit_click},
+			gap = "10",
+			alignment = "acenter",
+			margin = "10x10",
+			numdid = "8"
+			}]]--
+		;title="Results", 
+		font="Helvetica, 14",
+		size="800x200"
+		}
+	sec_dlg:showxy(iup.CENTER,iup.CENTER)
+	dlg:destroy()
 end
 
 function getIndexByValue(array, value) -- Возвращает индекс первого совпавшего значения в массиве 
-	for ind, val in pairs(array) do
+	for ind, val in ipairs(array) do
 		if val == value then
 			return ind
 		end	
@@ -131,66 +274,22 @@ function getIndexByValue(array, value) -- Возвращает индекс пе
 	return nil
 end
 
-function specFrac(frac_time, frac_high, frac_low, frac_interval) -- Определяет, что произошло раньше: low или high
-	local new_interval = intervals[getIndexByValue(intervals,frac_interval)-1]
-	local ds = CreateDataSource(p_classcode, p_seccode, new_interval)
-	local kindle, ind_high, ind_low
-	local try_count = 0	
-	while ds == nil and try_count < 100 do
-		sleep(100)
-		try_count = try_count + 1
-	end	
-	if ds ~= nil then
-		for i = 1, ds:Size() do
-			if ds:T(i) == frac_time then
-				kindle = i
-				break
-			end
-		end
-		if kindle ~= nil then
-			while kindle <= ds:Size() do
-				if ds:H(kindle) == frac_high then
-					ind_high = kindle
-				end
-				if ds:L(kindle) == frac_low then
-					ind_low = kindle
-				end
-				if ind_low ~= nil or ind_high ~= nil then -- На уровне ниже тоже может быть двунаправленный фрактал
-					break
-				end
-				kindle = kindle + 1
-			end
-			if ind_low ~= nil and ind_high ~= nil then
-				if ind_low < ind_high then
-					return "low"
-				elseif ind_low > ind_high then
-					return "high"
-				else -- Если это оказался двунаправленный фрактал, то снова переходим на уровень ниже
-					return specFrac(ds:T(ind_low), frac_low, frac_high, new_interval)
-				end
-			elseif ind_low ~= nil then
-				return "low"
-			elseif ind_high ~= nil then 
-				return "high"
-			end
-		end
-	else 
-		return nil
-	end
-end
-
 function saveToFile(str,...) -- По умолчанию сохраняет строку str в файл с названием fileName, можно вторым аргументом задать другое название
 	local file 
+	local mode = "a"
 	if arg.n == 0 then
-		file = io.open(filePath..fileName,"a") -- режим записи в файл с добавлением к содержимому файла 
+		file = io.open(filePath..fileName, mode) -- режим записи в файл с добавлением к содержимому файла		
 	else
-		file = io.open(filePath..arg[1],"a")
+		if arg[2] ~= nil then 
+			mode = arg[2]
+		end	
+		file = io.open(filePath..arg[1], mode)
 	end
 	file:write(str.."\n")
 	file:close()
 end
 
-function getFrac() -- Возвращает индексы вершин фракталов в обратном порядке
+function getFrac(interval) -- Возвращает индексы вершин фракталов в обратном порядке
 	-- Создаем таблицу для нижних и верхних фракталов (при этом порядок будет обратный, так как начинаем рассматривать интервал с конца)
 	local fractals = {
 		low = {},
@@ -242,7 +341,7 @@ function getFrac() -- Возвращает индексы вершин фрак�
 					tostring(ds:T(i - center).hour)..":"..tostring(ds:T(i - center).min)..
 					"\t\tLow: "..tostring(ds:L(i - center)))
 				if fractals.high[#fractals.high] == i - center then -- В случае двунаправленного фрактала 
-					local spec = specFrac(ds:T(i-center), ds:H(i-center), ds:L(i-center), p_interval)
+					local spec = specFrac(ds:T(i-center), ds:H(i-center), ds:L(i-center), interval)
 					if spec == "low" then
 						fractals.high[#fractals.high] = nil
 					elseif spec == "high" then
@@ -258,7 +357,7 @@ function getFrac() -- Возвращает индексы вершин фрак�
 	return fractals
 end	
 
-function defTrendWilliams(fractals) 
+function defTrendWilliams(fractals) -- Определение тренда по Вильямсу
 	local trend = "отсутствует"
 	-- Определение пробитий подряд для каждого вида фракталов
 	br = {
@@ -357,7 +456,7 @@ function defTrendWilliams(fractals)
 	return trend
 end
 
-function defTrendDow(fractals)
+function defTrendDow(fractals) -- Определение тренда по Доу
 	local trend = "отсутствует"
 	local hi = #fractals.high - 1 
 	local li = #fractals.low - 1
@@ -417,3 +516,104 @@ function defTrendDow(fractals)
 	end
 	return trend
 end
+
+function defTrendMA() -- Определение тренда по скользящим средним
+	local trend = "отсутствует"
+	local MA_f, MA_a, MA_s
+	for i = count - p_bars, count - 1 do
+		MA_f = SMA(i, p_MA_f, MA_f)
+		MA_a = SMA(i, p_MA_a, MA_a)
+		MA_s = SMA(i, p_MA_s, MA_s)
+		if MA_a <= MA_f and MA_a >= MA_s then -- Наблюдается тренд, если средняя MA располагается между двумя другими
+			if MA_f > math.max(MA_a, MA_s) and trend ~="восходящий" then
+				trend ="восходящий"
+			elseif MA_f < math.min(MA_a, MA_s) and trend ~="нисходящий" then
+				trend ="нисходящий"
+			end 
+		elseif trend ~="горизонтальный" then
+			trend ="горизонтальный"
+		end				
+	end
+	return trend
+end
+
+function SMA(index, period, prev_SMA) -- Простая скользящая средняя (SMA, Simple Moving Average)
+	if prev_SMA == nil then
+		local sum = 0
+		for i = index - period + 1, index do
+			sum = sum + ds:C(i)
+		end
+		return sum/period
+	else
+		return prev_SMA - (ds:C(index - period) - ds:C(index))/period
+	end
+end
+
+function specFrac(frac_time, frac_high, frac_low, frac_interval) -- Определяет, что произошло раньше: low или high
+	local new_interval = intervals[getIndexByValue(intervals,frac_interval)-1]
+	if new_interval == nil then
+		return nil
+	end
+	local ds = CreateDataSource(p_classcode, p_seccode, new_interval)
+	local kindle, ind_high, ind_low
+	local try_count = 0	
+	local seconds = os.time(frac_time)
+	while ds == nil and try_count < 100 do
+		sleep(100)
+		try_count = try_count + 1
+	end	
+	if ds ~= nil then
+		for i = 1, ds:Size() do
+			if os.time(ds:T(i)) == seconds then
+				kindle = i
+				break
+			end
+		end
+		if kindle ~= nil then
+			while kindle <= ds:Size() do
+				if ds:H(kindle) == frac_high then
+					ind_high = kindle
+				end
+				if ds:L(kindle) == frac_low then
+					ind_low = kindle
+				end
+				if ind_low ~= nil or ind_high ~= nil then -- На уровне ниже тоже может быть двунаправленный фрактал
+					break
+				end
+				kindle = kindle + 1
+			end
+			if ind_low ~= nil and ind_high ~= nil then
+				if ind_low < ind_high then
+					return "low"
+				elseif ind_low > ind_high then
+					return "high"
+				else -- Если это оказался двунаправленный фрактал, то снова переходим на уровень ниже
+					return specFrac(ds:T(ind_low), frac_low, frac_high, new_interval)
+				end
+			elseif ind_low ~= nil then
+				return "low"
+			elseif ind_high ~= nil then 
+				return "high"
+			end
+		end
+	else 
+		return nil
+	end
+end
+
+--[[
+function handleNewKindle (index) -- Обработка новой свечи
+	if index == count + 1 then -- При окончательном формировании последней свечи и появлении новой
+		local fractals = getFrac()
+		local new_trend_dow = defTrendDow(fractals)
+		local new_trend_williams = defTrendWilliams(fractals)
+		if new_trend_dow ~= trend_dow or new_trend_williams ~= trend_williams then
+			trend_dow = new_trend_dow
+			trend_williams = new_trend_williams
+			message("Направление тренда изменилось! Теперь он\n"..
+			"По Доу: "..trend_dow.."\n"..
+			"По Вильямсу: "..trend_williams)
+		end
+	end
+end
+]]--
